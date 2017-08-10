8074eb20c68472b492e37dc34b7dde32d98f5138
ldevos77 <ldevos77@gmail.com>
2017-08-10 18:46:05 +0200

    Creation of JAXBExampleProject project, chapter 9

---
+++ JAXBExampleProject/src/main/java/packt/jee/eclipse/jaxb/example/Teacher.java
@@ -0,0 +1,32 @@
+package packt.jee.eclipse.jaxb.example;
+
+public class Teacher {
+
+	private int id;
+	private String name;
+	
+	public Teacher() {
+		
+	}
+	
+	public Teacher (int id, String name) {
+		this.id=id;
+		this.name=name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
